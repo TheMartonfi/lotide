a0684a1d193e4c9598130de8e0735cbc099466a0
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-18 23:15:36 +0000

    Added file that contains a function that evaluates if arrays are identical or not

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  let allValues = '';
+  for (let i = 0; i <= actual.length - 1; i++) {
+    actual[i] === expected[i] ? allValues += 'true ' : allValues += 'false ';
+  }
+  return !allValues.includes('false') ? true : false;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
