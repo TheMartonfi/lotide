de60b18bfb3821cd1f1601c6b02d642daf6e9e68
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-18 23:27:27 +0000

    Added file that contains a function that logs if arrays are identical

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = function(actual, expected) {
+  let allValues = '';
+  for (let i = 0; i <= actual.length - 1; i++) {
+    actual[i] === expected[i] ? allValues += 'true ' : allValues += 'false ';
+  }
+  return !allValues.includes('false') ? true : false;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  eqArrays(arrOne, arrTwo) ? console.log(`🎉 Assertion Passed: ${arrOne} === ${arrTwo}`) : console.log(`⚠️  Assertion Failed: ${arrOne} !== ${arrTwo}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(['word', 2, 3], [1, 2, 3]);
+assertArraysEqual(['word', 2, 3], [1, 2, 3, 4]);
+assertArraysEqual(['word', 'not', 'life'], ['word', 'not', 'life']);
