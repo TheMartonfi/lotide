098077c73d5287f3bee8142b2f34119d808d0af5
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-24 20:07:51 +0000

    Added index.js that exports an object of all my functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -7,35 +7,35 @@ describe("#middle", () => {
     const input = [1, 2, 3];
     const expected = [2];
 
-    assert.sameOrderedMembers(middle(input), expected);
+    assert.deepEqual(middle(input), expected);
   });
 
   it("returns [] for [1, 2]", () => {
     const input = [1, 2];
     const expected = [];
 
-    assert.sameOrderedMembers(middle(input), expected);
+    assert.deepEqual(middle(input), expected);
   });
 
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
     const input = [1, 2, 3, 4, 5, 6];
     const expected = [3, 4];
 
-    assert.sameOrderedMembers(middle(input), expected);
+    assert.deepEqual(middle(input), expected);
   });
 
   it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
     const input = [1, 2, 3, 4, 5, 6, 7];
     const expected = [4];
 
-    assert.sameOrderedMembers(middle(input), expected);
+    assert.deepEqual(middle(input), expected);
   });
 
   it("returns [] for []", () => {
     const input = [];
     const expected = [];
 
-    assert.sameOrderedMembers(middle(input), expected); 
+    assert.deepEqual(middle(input), expected); 
   });
 
 });
